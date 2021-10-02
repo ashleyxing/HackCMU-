b6fe5a719d3704eb70d0b3db7f15d28b4649b6c0
uncleroach <ashley.s.xing@gmail.com>
2021-10-02 09:55:11 -0400

    using json as data

--- src/api/recipes/fetchWebsite.js
+++ src/api/recipes/fetchWebsite.js
@@ -4,30 +4,22 @@ const getIngredients = (recipe) => {
     console.log(recipe)
     return recipe.extendedIngredients.map((ingredient) => [ingredient.name, ingredient.measures.metric.amount, ingredient.measures.metric.unitLong]);
 };
-const getSubstitute = async (ingredient) => {
-    var subs = await fetch(`https://api.spoonacular.com/food/ingredients/substitutes?apiKey=6b87762d3baa4f7d911132a401c70e65&ingredientName=${ingredient}`)
-        .then((response) => response.json())
-        .then((data) => data.substitutes);
-    console.log(subs);
-    return subs
+const getSubstitute = (json, ingredient) => {
+    let name = ingredient.name
+    console.log(name)
+    console.log(json.substitutes)
+    return json.substitutes[name];
 };
-const getSubstitutes = async (ingredients) => {
-    return await ingredients.map(async (ingredient) => await getSubstitute(ingredient))
+const getSubstitutes = (json, ingredients) => {
+    return ingredients.map((ingredient) => getSubstitute(json, ingredient))
 };
 const fetchWebsite = async () => {
     var json = require('../../data/burger.json');
     var ingredients = json.ingredients;
-    // var ingredients = await fetch('https://localhost:3001', {
-    //     headers: {
-    //         'Content-Type':'application/json',
-    //     },
-    // })
-    //     .then((data) => data.ingredients)
-    //     .catch((error) => console.log(error));
     console.log(ingredients)
-    // var subs = getSubstitutes(ingredients);
-    // console.log([ingredients,subs]);
-    // return [ingredients, subs];
+    var subs = getSubstitutes(json, ingredients);
+    console.log([ingredients,subs]);
+    return [ingredients, subs];
 }
 
 export default fetchWebsite;
