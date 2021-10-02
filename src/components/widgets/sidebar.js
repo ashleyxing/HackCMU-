cb1d3e6cb934102a43711d3e9cbbe9762f872521
uncleroach <ashley.s.xing@gmail.com>
2021-10-01 23:50:25 -0400

    for you jimmy

---
+++ src/components/widgets/sidebar.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+// import { Link } from 'react-router-dom';
+import fetchWebsite from '../../api/recipes/fetchWebsite';
+
+const Sidebar = () => {
+  const fetchWebsite = () => {
+    let recipe;
+    fetch('https://api.spoonacular.com/recipes/extract?apiKey=6b87762d3baa4f7d911132a401c70e65&url=https://foodista.com/recipe/ZHK4KPB6/chocolate-crinkle-cookies')
+        .then((response) => response.json())
+        .then((data) => {
+            recipe = data;
+            return recipe;
+        })
+        .catch((error) => console.log("fuck"));
+    return recipe;
+}
+
+
+  return (
+    <SidebarWrapper>
+      <div className="about-container">
+        <h1 className="clean-plate-title">Clean Plate</h1>
+        <p className="description">A fresh and environmentally friendly way of finding recipes and substitutes. Try it out now!</p>
+        <button onClick={fetchWebsite}>PLEASE WORK</button>
+      </div>
+    </SidebarWrapper>
+  );
+};
+
+const SidebarWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  right: 552px;
+  bottom: 0px;
+  z-index: 0;
+  width: 40%;
+  padding: 50px;
+  background-color: #dcdcdc;
+
+  .about-container {
+    padding-right: 75px;
+  }
+
+  .clean-plate-title {
+    font-size: 50px;
+    font-weight: 700;
+  }
+
+  .description {
+    margin-bottom: 40px;
+  }
+
+  .button {
+    color: white;
+    background-color: rgb(122, 155, 118);
+    border-radius: 4px;
+    padding: 0.75rem;
+    font-size: 18px;
+    text-decoration: none;
+    cursor: pointer;
+    transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
+
+    &:hover {
+      transform: none;
+      background-color: rgba(122, 155, 118, 0.7);
+      outline: none;
+    }
+  }
+`; 
+
+export default Sidebar;
