5520df6858274a867031f1aa5daf15ca8ff3babb
jimbolee28 <jimmyzhang7528@gmail.com>
2021-10-01 23:52:08 -0400

    Minor fixes

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Sidebar from '../widgets/sidebar';
 import VerticalScroll from '../widgets/verticalscroll';
